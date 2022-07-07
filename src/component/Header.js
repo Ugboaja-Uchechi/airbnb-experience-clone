51a5b3820f8a0aed6d824259ad9b8417ad4af4b0
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-07-07 16:39:38 +0100

    Create image and header

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from '../Images/airbnb.png'
+
+const Header = () => {
+  <>
+    <header>
+      <div>
+        <img src={Logo} alt="airbnb logo" />
+      </div>
+    </header>
+  </>
+}
+
+export default Header;
