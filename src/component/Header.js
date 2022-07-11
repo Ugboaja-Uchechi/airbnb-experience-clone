e7dba2239eec15ab823330e49db59552c1ef487a
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-07-11 11:24:03 +0100

    Install and import icons

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,14 +1,27 @@
 import React from "react";
-import Logo from '../Images/airbnb.png'
+import { FaGlobe } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { SiAirbnb } from "react-icons/si";
+import { IoIosMenu } from "react-icons/io";
 
 const Header = () => {
-  <>
-    <header>
-      <div>
-        <img src={Logo} alt="airbnb logo" />
-      </div>
-    </header>
+  return (
+    <>
+      <header>
+        <nav className="nav-flex">
+          <div>
+            <SiAirbnb className="logo" />
+          </div>
+          <div>
+            <span>Become a host</span>
+            <FaGlobe />
+            <IoIosMenu />
+            <FaUserCircle />
+          </div>
+        </nav>
+      </header>
   </>
+  )
 }
 
 export default Header;
