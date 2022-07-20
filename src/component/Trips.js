9a0f63aebe16185aaad87784f64fd7e03d70e9e6
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-07-20 22:26:21 +0100

    Rename images and add to component

--- src/component/Trips.js
+++ src/component/Trips.js
@@ -1,10 +1,32 @@
 import React from "react"
+import Man from "./Images/man.jpg"
+import Pyramid from "./Images/pyramid.jpg"
+import Woman from "./Images/woman.jpg"
+import Boat from "./Images/boat.jpg"
+import Beach from "./Images/beach.jpg"
 
 const Trip = () => {
   return (
     <>
       <section>
         <h2>Plan a trip with help from local Hosts around the world</h2>
+        <div>
+          <div>
+            <img src={Man} alt="A man standing on a rock looking at mountains"/>
+          </div>
+          <div>
+            <img src={Pyramid} alt="A man standing on a rock looking at mountains"/>
+          </div>
+          <div>
+            <img src={Woman} alt="A woman standing on a rock"/>
+          </div>
+          <div>
+            <img src={Boat} alt="A woman with legs crossed sitting in a boat"/>
+          </div>
+          <div>
+            <img src={Beach} alt="Tourists on a beach"/>
+          </div>
+        </div>
       </section>
     </>
   )
