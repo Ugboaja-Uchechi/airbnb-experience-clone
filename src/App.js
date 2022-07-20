49209563072ce60667ed7f8f6976954375e6a29f
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-07-20 22:07:53 +0100

    Add images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './component/Header';
 import Main from './component/Main';
+import Trip from './component/Trips';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Trip />
     </>
   );
 }
---
+++ src/component/Trips.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Trip = () => {
+  return (
+    <>
+      <section>
+        <h2>Plan a trip with help from local Hosts around the world</h2>
+      </section>
+    </>
+  )
+}
+
+export default Trip;
