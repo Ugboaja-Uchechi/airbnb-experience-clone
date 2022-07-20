7aca63ae1794ebf819897e81d228592ebb302ab4
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-07-20 23:03:42 +0100

    Create trips data dynamically

--- src/component/Trips.js
+++ src/component/Trips.js
@@ -1,31 +1,26 @@
 import React from "react"
-import Man from "./Images/man.jpg"
-import Pyramid from "./Images/pyramid.jpg"
-import Woman from "./Images/woman.jpg"
-import Boat from "./Images/boat.jpg"
-import Beach from "./Images/beach.jpg"
+import { FaStar} from "react-icons/fa";
+// import TripsData from "./TripsData"
 
-const Trip = () => {
+const Trip = (props) => {
   return (
     <>
       <section>
         <h2>Plan a trip with help from local Hosts around the world</h2>
         <div>
           <div>
-            <img src={Man} alt="A man standing on a rock looking at mountains"/>
+            <img src={`./Images/${props.img}`} alt={props.alt} />
           </div>
-          <div>
-            <img src={Pyramid} alt="A man standing on a rock looking at mountains"/>
-          </div>
-          <div>
-            <img src={Woman} alt="A woman standing on a rock"/>
-          </div>
-          <div>
-            <img src={Boat} alt="A woman with legs crossed sitting in a boat"/>
-          </div>
-          <div>
-            <img src={Beach} alt="Tourists on a beach"/>
-          </div>
+          <div className="card--stats">
+          <FaStar />
+            <span>{props.stats.rating}</span>
+              <span className="gray">({props.stats.count}) • </span>
+              <span className="gray">{props.country}</span>
+            </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price">
+                <span className="bold">From ${props.amount}</span> / person
+            </p>
         </div>
       </section>
     </>
---
+++ src/component/TripsData.js
@@ -0,0 +1,77 @@
+const TripsData = [
+  {
+    id: 1,
+    img: "man.jpg",
+    title: "A trip to the mountains",
+    stats: {
+      rating: 4.5,
+      count: 10,
+    },
+    country: "Russia",
+    amount: 10,
+    alt: "A man standing on a rock looking at mountains"
+  },
+  {
+    id: 2,
+    img: "pyramid.jpg",
+    title: "Come see the pyramids",
+    stats: {
+      rating: 4.9,
+      count: 25,
+    },
+    country: "Egypt",
+    amount: 15,
+    alt: "A man walking towards the pyramids"
+  },
+  {
+    id: 3,
+    img: "woman.jpg",
+    title: "Come see the greenlands",
+    stats: {
+      rating: 5,
+      count: 15,
+    },
+    country: "Austria",
+    amount: 9,
+    alt: "A woman standing on a rock"
+  },
+  {
+    id: 4,
+    img: "boat.jpg",
+    title: "Take a boat cruise with us",
+    stats: {
+      rating: 4.8,
+      count: 10,
+    },
+    country: "Cape Verde",
+    amount: 10,
+    alt: "A woman with legs crossed sitting in a boat"
+  },
+  {
+    id: 5,
+    img: "beach.jpg",
+    title: "Take a vacation",
+    stats: {
+      rating: 4.3,
+      count: 20,
+    },
+    country: "U.S.A",
+    amount: 25,
+    alt: "Tourists on a beach"
+  },
+  {
+    id: 6,
+    img: "nature.jpg",
+    title: "Take a hike through nature",
+    stats: {
+      rating: 4.9,
+      count: 30,
+    },
+    country: "Vietnam",
+    amount: 10,
+    alt: "Women hiking through nature"
+  }
+  
+]
+
+export default TripsData;
