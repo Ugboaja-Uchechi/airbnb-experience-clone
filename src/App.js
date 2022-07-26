1a71d07a0dac05f9717d6ad4656cc3e3c7ccb56d
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-07-26 16:50:39 +0100

    Create component and map through created data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Trip from './component/Trips';
 import TripsData from './component/TripsData';
 import TripsHeader from './component/TripsHeader';
 import Footer from './component/Footer';
+import Weekend from './component/Weekend';
+import WeekendData from './component/WeekendData';
 
 function App() {
   const trips = TripsData.map(data => {
@@ -15,12 +17,22 @@ function App() {
       />
     )
   })
+
+  const weekend = WeekendData.map(items => {
+    return (
+      <Weekend
+        key={items.id}
+        {...items}
+      />
+    )
+  })
   return (
     <>
       <Header />
       <Main />
       <TripsHeader />
       {trips}
+      {weekend}
       <Footer />
     </>
   );
---
+++ src/component/Weekend.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+import { FaStar} from "react-icons/fa";
+
+const Weekend = (props) => {
+  return (
+    <>
+    <section>
+      <div>
+          <img src={props.img} alt={props.alt} />
+
+        <div>
+        <FaStar />
+          <span>{props.stats.rating}</span>
+            <span>({props.stats.count}) • </span>
+            <span>{props.country}</span>
+          </div>
+          <p>{props.title}</p>
+          <p>
+              <span>From ${props.amount}</span> / person
+          </p>
+      </div>
+    </section>
+  </>
+  )
+}
+
+export default Weekend;
