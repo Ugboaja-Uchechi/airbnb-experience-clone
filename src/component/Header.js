07378d3bc314aa7ec814d2ffa395a6d71193e45f
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-07-27 00:27:43 +0100

    Style header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaGlobe } from "react-icons/fa";
 import { FaUserCircle } from "react-icons/fa";
+// import { FaBars } from "react-icons/fa";
 import { SiAirbnb } from "react-icons/si";
 import { IoIosMenu } from "react-icons/io";
 
@@ -9,14 +10,17 @@ const Header = () => {
     <>
       <header>
         <nav className="nav-flex">
-          <div>
+          <div className="logo-container">
             <SiAirbnb className="logo" />
+            <span className="logo-span">airbnb</span>
           </div>
           <div>
-            <span>Become a host</span>
-            <FaGlobe />
-            <IoIosMenu />
-            <FaUserCircle />
+            <span className="header-span">Become a Host</span>
+            <FaGlobe className="icons" />
+            <div className="icon-container">
+              <IoIosMenu className="icons menu" />
+              <FaUserCircle className="icons user" />
+            </div>
           </div>
         </nav>
       </header>
